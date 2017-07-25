7d33e265fa7a58a98e201fe03a97952709788d15
cstin854 <cstingley@deforestschools.org>
2017-07-24 19:10:07 -0500

    Added "is_favorite" attribute to Song class.

---
+++ mysite/music/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Album
+
+
+def index(request):
+    all_albums = Album.objects.all()
+    context = {
+        'all_albums' : all_albums
+        }
+    return render(request, 'music/index.html', context)
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album' : album})
